e72f77ff86c17a2e0c6a7a522b5b4c73b8d12f1b
karmaecrivain94 <karmaecrivain94@users.noreply.github.com>
2017-04-18 00:28:04 +0200

    Implementing database subdomain + .html files -> .ejs files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # DatabaseUI
+
+## Home page
+Navigate various data visualizations about governments, upcoming elections, etc...
+
+* url: `http://database.acteuropa.eu/`
+## Country page
+
+Details about the specified EU country
+* url: `country/<name>`
+
+* example: `http://database.acteuropa.eu/country/france`
+
+***
+
+## Person page
+
+Details about european politicians
+* url: `person/<name>`
+
+* example: `http://database.acteuropa.eu/person/Emmanuel_Macron`
+
+***
+
+## Political party page
+
+Details about political parties
+
+* url: `/party/<country>/<name>`
+
+* example: `http://database.acteuropa.eu/party/france/em`
--- main.js
+++ main.js
@@ -1,16 +1,47 @@
 var express = require('express')
+var subdomain = require('express-subdomain');
 var app = express()
 
-app.use(express.static('public'))
-app.get("/", function(req, res, next) {
-    res.sendfile("public/html/index.html")
+app.use(express.static('public'));
+
+
+
+//DATABASE ROUTER
+
+var dbRouter = express.Router(); 
+app.use(subdomain('database', dbRouter));
+
+dbRouter.get("/", function(req, res, next) {
+    res.sendFile("views/index.ejs", { root : __dirname})
 })
-app.get("/p", function(req, res, next) {
-    res.sendfile("public/html/PoliticianPage.html")
+dbRouter.get("/person/:name", function(req, res, next) {
+    console.log("Person=" + req.params.name)
+    res.render('PoliticianPage.ejs', {person_name: req.params.name});
 })
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!')
+dbRouter.get("/party/:country/:name", function(req, res, next) {
+    console.log("Country=" + req.params.country)
+    console.log("Party=" + req.params.name)
+    res.sendFile("views/PoliticalParty.ejs", { root : __dirname})
 })
+dbRouter.get("/country/:name", function(req, res, next) {
+    console.log("Country=" + req.params.name)
+    res.sendFile("public/html/CountryPage.html", { root : __dirname})
+})
+
+//MAIN PAGE ROUTER
+var hRouter = express.Router();
+app.use(hRouter);
+//TODO add website root
+hRouter.get("/", function(req, res, next) {
+    res.send("website root");
+})
+
+//404 page
 app.use(function (req, res, next) {
-  res.status(404).sendfile("public/html/404.html")
+  res.status(404).sendFile("public/html/404.html", { root : __dirname})
+})
+
+//Run server
+app.listen(80, function () {
+  console.log('ActEuropa listening on port 80!')
 })
